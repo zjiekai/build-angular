cb5d900d3ac8dc1a513f1febc0b04ecac20dc270
zjk <jiekai.zheng@transwarp.io>
2016-05-31 15:34:00 +0800

    string literal

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('lodash');
+
 function parse(expr) {
   var lexer = new Lexer();
   var parser = new Parser(lexer);
@@ -17,14 +19,22 @@ Lexer.prototype.lex = function(text) {
 
   while (this.index < this.text.length) {
     this.ch = this.text.charAt(this.index);
-    if (this.isNumber(this.ch)) {
+    if (this.isNumber(this.ch) || 
+        (this.ch === '.' && this.isNumber(this.peek()))) {
       this.readNumber();
+    } else if (this.ch === '\'' || this.ch === '"') {
+      this.readString();
     } else {
       throw 'Unexpected next character: ' + this.ch;
     }
   }
 
   return this.tokens;
+};
+
+Lexer.prototype.peek = function() {
+  return this.index < this.text.length - 1 ?
+    this.text.charAt(this.index + 1) : false;
 };
 
 Lexer.prototype.isNumber = function(ch) {
@@ -46,6 +56,26 @@ Lexer.prototype.readNumber = function() {
     text: number,
     value: Number(number)
   });
+};
+
+Lexer.prototype.readString = function() {
+  this.index++;
+  var string = '';
+  while (this.index < this.text.length) {
+    var ch = this.text.charAt(this.index);
+    if (ch === '\'' || ch === '"') {
+      this.index++;
+      this.tokens.push({
+        text: string,
+        value: string
+      });
+      return;
+    } else {
+      string += ch;
+    }
+    this.index++;
+  }
+  throw 'Unmatched quote';
 };
 
 function AST(lexer) {
@@ -86,7 +116,15 @@ ASTCompiler.prototype.recurse = function(ast) {
       this.state.body.push('return ', this.recurse(ast.body), ';');
       break;
     case AST.Literal:
-      return ast.value;
+      return this.escape(ast.value);
+  }
+};
+
+ASTCompiler.prototype.escape = function(value) {
+  if (_.isString(value)) {
+    return '\'' + value + '\'';
+  } else {
+    return value;
   }
 };
 
--- test/parse_spec.js
+++ test/parse_spec.js
@@ -15,4 +15,18 @@ describe('parse', function() {
     expect(fn()).toBe(4.2);
   });
 
+  it('can parse a floating point number without an integer part', function() {
+    var fn = parse('.42');
+    expect(fn()).toBe(0.42);
+  });
+
+  it('can parse a string in single quotes', function() {
+    var fn = parse("'abc'");
+    expect(fn()).toEqual('abc');
+  });
+
+  it('can parse a string in double quotes', function() {
+    var fn = parse('"abc"');
+    expect(fn()).toEqual('abc');
+  });
 });
