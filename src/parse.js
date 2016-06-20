6dbaaff21b4f7cf1b496f7f13e02031b091a25eb
zjk <jiekai.zheng@transwarp.io>
2016-06-20 16:04:40 +0800

    parse looks up a 4-part identifier path

--- src/parse.js
+++ src/parse.js
@@ -181,7 +181,7 @@ AST.prototype.primary = function() {
   } else {
     primary = this.constant();
   }
-  if (this.expect('.')) {
+  while (this.expect('.')) {
     primary = {
       type: AST.MemberExpression,
       object: primary,
