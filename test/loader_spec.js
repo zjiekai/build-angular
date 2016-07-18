37ae6c6ee4c9e870cd71f41fa6e2d9f45338329a
zjk <jiekai.zheng@transwarp.io>
2016-07-18 11:40:36 +0800

    delete window.angular

--- test/loader_spec.js
+++ test/loader_spec.js
@@ -4,6 +4,10 @@ var setupModuleLoader = require('../src/loader');
 
 describe('setupModuleLoader', function() {
 
+    beforeEach(function() {
+        delete window.angular;
+    });
+
     // pg401
     it('exposes angular on the window', function() {
         setupModuleLoader(window);
@@ -28,43 +32,44 @@ describe('setupModuleLoader', function() {
         setupModuleLoader(window);
         expect(window.angular.module).toBe(module);
     });
+
+    describe('modules', function() {
+
+        beforeEach(function() {
+            setupModuleLoader(window);
+        });
+
+        it('allows registering a module', function() {
+            var myModule = window.angular.module('myModule', []);
+            expect(myModule).toBeDefined();
+            expect(myModule.name).toEqual('myModule');
+        });
+
+        it('replaces a module when registered with same name again', function() {
+            var myModule = window.angular.module('myModule', []);
+            var myNewModule = window.angular.module('myModule', []);
+            expect(myNewModule).not.toBe(myModule);
+        });
+
+        it('attaches the requires array to the registered module', function() {
+            var myModule = window.angular.module('myModule', ['myOtherModule']);
+            expect(myModule.requires).toEqual(['myOtherModule']);
+        });
+
+        it('allows getting a module', function() {
+            var myModule = window.angular.module('myModule', []);
+            var gotModule = window.angular.module('myModule');
+
+            expect(gotModule).toBeDefined();
+            expect(gotModule).toBe(myModule);
+        });
+
+        it('throws when trying to get a nonexistent module', function() {
+            expect(function() {
+                window.angular.module('myModule');
+            }).toThrow();
+        });
+    });
 });
 
-describe('modules', function() {
 
-    beforeEach(function() {
-        delete window.angular;
-        setupModuleLoader(window);
-    });
-
-    it('allows registering a module', function() {
-        var myModule = window.angular.module('myModule', []);
-        expect(myModule).toBeDefined();
-        expect(myModule.name).toEqual('myModule');
-    });
-
-    it('replaces a module when registered with same name again', function() {
-        var myModule = window.angular.module('myModule', []);
-        var myNewModule = window.angular.module('myModule', []);
-        expect(myNewModule).not.toBe(myModule);
-    });
-
-    it('attaches the requires array to the registered module', function() {
-        var myModule = window.angular.module('myModule', ['myOtherModule']);
-        expect(myModule.requires).toEqual(['myOtherModule']);
-    });
-
-    it('allows getting a module', function() {
-        var myModule = window.angular.module('myModule', []);
-        var gotModule = window.angular.module('myModule');
-
-        expect(gotModule).toBeDefined();
-        expect(gotModule).toBe(myModule);
-    });
-
-    it('throws when trying to get a nonexistent module', function() {
-        expect(function() {
-            window.angular.module('myModule');
-        }).toThrow();
-    });
-});
