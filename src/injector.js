c623360b68f32208c041f921f4e3c3b2ceb10c2f
zjk <jiekai.zheng@transwarp.io>
2016-07-20 10:29:44 +0800

    pass constant tests

--- src/injector.js
+++ src/injector.js
@@ -1,7 +1,30 @@
 'use strict';
 
+var _ = require('lodash');
+
 function createInjector(modulesToLoad) {
-    return {};
+    var cache = {};
+
+    var $provide = {
+        constant: function(key, value) {
+            cache[key] = value;
+        }
+    };
+
+    _.forEach(modulesToLoad, function(moduleName) {
+        var module = window.angular.module(moduleName);
+        _.forEach(module._invokeQueue, function(invokeArgs) {
+            var method = invokeArgs[0];
+            var args = invokeArgs[1];
+            $provide[method].apply($provide, args);
+        });
+    });
+
+    return {
+        has: function(key) {
+            return cache.hasOwnProperty(key);
+        }
+    };
 }
 
 module.exports = createInjector;
