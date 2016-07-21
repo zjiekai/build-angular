ef3e1c89ab31c96d2c6f308fdb0d19c5489ed464
zjk <jiekai.zheng@transwarp.io>
2016-07-21 10:35:18 +0800

    throw when inject non-string token

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -93,6 +93,19 @@ describe('injector', function() {
         expect(injector.invoke(fn)).toBe(3);
     });
 
+    it('does not accept non-strings as injection tokens', function() {
+        var module = window.angular.module('myModule', []);
+        module.constant('a', 1);
+        var injector = createInjector(['myModule']);
+
+        var fn = function(one, two) { return one + two; };
+        fn.$inject = ['a', 2];
+
+        expect(function() {
+            injector.invoke(fn);
+        }).toThrow();
+    });
+
     // pg419
     it('overrides dependencies with locals when invoking', function() {
         
