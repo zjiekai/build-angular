c9ec36accb9c968eebae3beebe34709742fd06a9
zjk <jiekai.zheng@transwarp.io>
2016-08-11 16:55:41 +0800

    create a deferred

--- src/q.js
+++ src/q.js
@@ -4,7 +4,19 @@ function $QProvider() {
 
     this.$get = function() {
 
+        function Deferred() {
+
+        }
+
+        function defer() {
+            return new Deferred();
+        }
+
+        return {
+            defer: defer
+        };
+
     };
 }
 
-module.exports = $QProvider;
+module.exports = [$QProvider];
