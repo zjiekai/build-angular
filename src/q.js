332ee3ea678407b5de74c7a89991daf369b6198d
zjk <jiekai.zheng@transwarp.io>
2016-08-12 10:39:45 +0800

    resolve tests

--- src/q.js
+++ src/q.js
@@ -5,12 +5,20 @@ function $QProvider() {
     this.$get = function() {
 
         function Promise() {
-
+            this.$$state = {};
         }
+
+        Promise.prototype.then = function(onFulfilled) {
+            this.$$state.pending = onFulfilled;
+        };
 
         function Deferred() {
             this.promise = new Promise();
         }
+
+        Deferred.prototype.resolve = function(value) {
+            this.promise.$$state.pending(value);
+        };
 
         function defer() {
             return new Deferred();
--- test/q_spec.js
+++ test/q_spec.js
@@ -22,4 +22,44 @@ describe('$q', function() {
         var d = $q.defer();
         expect(d.promise).toBeDefined();
     });
+
+    it('can resolve a promise', function(done) {
+
+        var deferred = $q.defer();
+        var promise = deferred.promise;
+
+        var promiseSpy = jasmine.createSpy();
+        promise.then(promiseSpy);
+
+        deferred.resolve('a-ok');
+
+        setTimeout(function() {
+            expect(promiseSpy).toHaveBeenCalledWith('a-ok');
+            done();
+        }, 0.1);
+    });
+
+    it('works when resolved before promise listener', function(done) {
+        var d = $q.defer();
+        d.resolve(42);
+
+        var promiseSpy = jasmine.createSpy();
+        d.promise.then(promiseSpy);
+
+        setTimeout(function() {
+            expect(promiseSpy).toHaveBeenCalledWith(42);
+            done();
+        }, 0);
+    });
+
+    it('does not resolve promise immediately', function() {
+        var d = $q.defer();
+
+        var promiseSpy = jasmine.createSpy();
+        d.promise.then(promiseSpy);
+
+        d.resolve(42);
+
+        expect(promiseSpy).not.toHaveBeenCalled();
+    });
 });
